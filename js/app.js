3908ac123a266b20f7c1137e087b2012b2da8790
Rahul2360 <rahulbansal2360@gmail.com>
2017-03-04 18:26:40 +0530

    javascript file added

---
+++ js/app.js
@@ -0,0 +1,19 @@
+window.onload = function() {
+     
+    var current,screen,output,limit,zero,period,operator;
+    screen = document.getElementById("result");
+    
+    var elem = document.querySelectorAll(".num");
+    var len = elem.length;
+    for(var i=0;i<len;i++) {
+        elem[i].addEventListener("click",function() {
+            num= this.value;
+            output = screen.innerHTML +=num;
+            limit = output.length;
+            if(limit>16) {
+                alert("No more input allowed");
+            }
+        },false);
+    }
+    
+}
