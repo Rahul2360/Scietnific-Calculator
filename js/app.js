ba5122e9dd69dc9ec675e62b8bb53fe6329d4c9a
Rahul2360 <rahulbansal2360@gmail.com>
2017-03-07 21:52:10 +0530

    javascript added

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 window.onload = function() {
      
-    var current,screen,output,limit,zero,period,operator;
+    var screen,output,limit,operator;
     screen = document.getElementById("result");
     
     var elem = document.querySelectorAll(".num");
@@ -16,4 +16,54 @@ window.onload = function() {
         },false);
     }
     
+    document.querySelector(".zero").addEventListener("click", function() {
+        var zero = this.value;
+        if(screen.innerHTML === "") {
+            output = screen.innerHTML = zero;
+        }
+        else if (screen.innerHTML === output) {
+            output = screen.innerHTML += zero;
+        }
+        else if(screen.innerHTML === (output +".")) {
+            output = screen.innerHTML +=zero;
+        }
+    } ,false);
+    
+    document.querySelector(".dot").addEventListener("click",function() {
+        var dot = this.value;
+        if(screen.innerHTML === ""){
+            output = screen.innerHTML = screen.innerHTML.concat("0.");
+        }
+        else if(screen.innerHTML === output) {
+            screen.innerHTML = screen.innerHTML.concat(".");
+        }
+    },false);
+    
+    document.querySelector(".answer").addEventListener("click",function() {
+        if(screen.innerHTML === output) {
+            screen.innerHTML = eval(output);
+        }
+        else {
+            screen.innerHTML = "";
+        }
+    },false);
+    
+    document.querySelector(".start").addEventListener("click",function() {
+        screen.innerHTML = "";
+    },false);
+    
+    var elem1 = document.querySelectorAll(".operator");
+    var len1 = elem1.length;
+    for(var j=0;j<len1;j++){
+        elem1[j].addEventListener("click",function() {
+            operator = this.value;
+            if(screen.innerHTML === "") {
+                screen.innerHTML = screen.innerHTML.concat("");
+            }
+            else if(output) {
+                screen.innerHTML = output.concat(operator);
+            }
+        },false);
+    }
 }
+
